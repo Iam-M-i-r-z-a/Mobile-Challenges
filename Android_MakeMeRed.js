34d4cfbe35af96fa8afdca483f149a904982074d
MIЯZΛ <104619151+Iam-M-i-r-z-a@users.noreply.github.com>
2024-09-01 11:15:07 +0300

    Add files via upload

---
+++ Android_MakeMeRed.js
@@ -0,0 +1,6 @@
+Java.perform(function() {
+    let MainActivity = Java.use("com.example.tryhookme.MainActivity");
+    MainActivity.fun.implementation = function(Param1) {
+      	this.fun("Reeeeeeeeeeeeeeeeeeeeeeed")
+    };
+  });
