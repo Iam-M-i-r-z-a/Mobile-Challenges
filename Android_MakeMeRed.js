ab29578b2f57a6eb353916e80993e20f6bcca99a
MIЯZΛ <104619151+Iam-M-i-r-z-a@users.noreply.github.com>
2024-09-02 20:32:59 +0300

    Delete Android_MakeMeRed.js

--- Android_MakeMeRed.js
+++
@@ -1,6 +0,0 @@
-Java.perform(function() {
-    let MainActivity = Java.use("com.example.tryhookme.MainActivity");
-    MainActivity.fun.implementation = function(Param1) {
-      	this.fun("Reeeeeeeeeeeeeeeeeeeeeeed")
-    };
-  });
