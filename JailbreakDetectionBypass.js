85832e41dad80614a7d3acb05efbeb73c1c5ed33
MIЯZΛ <104619151+Iam-M-i-r-z-a@users.noreply.github.com>
2024-09-05 21:36:28 +0300

    Add files via upload

---
+++ JailbreakDetectionBypass.js
@@ -0,0 +1,48 @@
+if (ObjC.available) {
+    var paths = [
+        "/Applications/Cydia.app",
+        "/etc/ssh/sshd_config",
+        "/user/lib/substrate"
+    ];
+
+    var f = Module.findExportByName("libSystem.B.dylib", "stat64");
+    Interceptor.attach(f, {
+        onEnter: function(args) {
+            this.is_common_path = false;
+            var arg = Memory.readUtf8String(args[0]);
+            for (var path in paths) {
+                if (arg.indexOf(paths[path]) > -1) {
+                    console.log("Hooking native function stat64: " + arg);
+                    this.is_common_path = true;
+                    //return -1;
+                }
+            }
+        },
+        onLeave: function(retval) {
+            if (this.is_common_path) {
+                console.log("stat64 Bypass!!!");
+                retval.replace(-1);
+            }
+        }
+    });
+    var f = Module.findExportByName("libSystem.B.dylib", "stat");
+    Interceptor.attach(f, {
+        onEnter: function(args) {
+            this.is_common_path = false;
+            var arg = Memory.readUtf8String(args[0]);
+            for (var path in paths) {
+                if (arg.indexOf(paths[path]) > -1) {
+                    console.log("Hooking native function stat: " + arg);
+                    this.is_common_path = true;
+                    //return -1;
+                }
+            }
+        },
+        onLeave: function(retval) {
+            if (this.is_common_path) {
+                console.log("stat Bypass!!!");
+                retval.replace(-1);
+            }
+        }
+    });
+}
